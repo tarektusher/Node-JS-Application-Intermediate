50e1e8bbf104705b01d731286f7e600c19aa908d
Md Tarek Hossain Tusher <57767316+tarektusher@users.noreply.github.com>
2023-08-04 15:15:30 +0600

    I am doing CRUD operation using Mongodb Atlas

--- server.js
+++ server.js
@@ -6,11 +6,8 @@ const mongoose = require('mongoose');
 
 const app = express();
 app.use(bodyParser.json());
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 6000;
 
-//? Create a object and id
-// let users = [];
-let cc=0;
 
 //?MongoDB Connect 
 const url = process.env.MONGODB_URI;
@@ -25,14 +22,18 @@ mongoose.connection.on('error',(err)=>{
 });
 
 //? MongoDB Schema
-const Schema = mongoose.Schema;
-const objectId = Schema.ObjectId;
-const users = new Schema ({
+const userSchema = new mongoose.Schema(
+     {
      fname : String,
      lname : String,
      email : String,
-     id : String,
-})
+     age : Number
+     },
+     {
+          timestamps : true
+     }
+)
+const User = mongoose.model('User',userSchema);
 
 //? API to check connection
 app.get('/',(req,res) =>{
@@ -41,59 +42,74 @@ app.get('/',(req,res) =>{
 
 
 //? API to create a User
-app.post('/users',(req,res) => {
-     const user = req.body;
-     user.id = ++cc;
-     users.push(user);
-     res.status(201).json(user);
+app.post('/users',async (req,res) => {
+     try {
+          const user = new User(req.body);
+          await user.save();
+          res.status(201).json(user);
+     } catch (error) {
+          console.error(error);
+          res.status(500).json({message : `Something is worng in the server`});
+     }
 })
 
 //? API to get Users 
-app.get('/users',(req,res) =>{
-     res.status(200).json(users);
+app.get('/users',async(req,res) =>{
+     try {
+          const users =  await User.find({})
+          res.json(users);
+          
+     } catch (error) {
+          res.status(404).json(`User not Found`);
+     }
 })
 
 //? API to get specfic user
-app.get('/users/:id',(req,res)=>{
-     const id =req.params.id;
-     const user = users.find((u) => u.id ==  id);
-     if(user){
-          res.json(user);
-     }
-     else{
-          res.status(404).json({message:"User Not Found"});
+app.get('/users/:id',async (req,res)=>{
+     try {
+          const id =req.params.id;
+          const user = await User.findById(id);
+          if(user){
+               res.json(user);
+          }
+          else{
+               res.status(404).json({message:"User Not Found"});
+          }
+     } catch (error) {
+          res.status(500).json({message : `SomeThing wrong in Server`});
      }
 })
 
 //? API to Update a user
-app.put('/users/:id',(req,res) =>{
-     const id = req.params.id;
-     const user = users.find((u) => u.id ==id);
-     if(user){
-          // user.fname = "Afroza";
-          // user.lname = "Farzana";
-          // user.email = "afrozalimacox@gmail.com";
-          const body =req.body;
-          user.fname = body.fname;
-          user.lname = body.lname;
-          user.email = body.email;
-          res.json(user);
-     }
-     else {
-          res.status(404).json({message:"User Not Found"});
+app.put('/users/:id',async(req,res) =>{
+     try {
+          const id = req.params.id;
+          const body = req.body
+          const user = await User.findByIdAndUpdate(id,body,{new : true});
+          if(user){
+               res.json(user);
+          }
+          else {
+               res.status(404).json({message:"User Not Found"});
+          }
+     } catch (error) {
+          res.status(500).json({message : `SomeThing wrong in Server`});
      }
 })
 
 //? API to DELETE a user
-app.delete('/users/:id',(req,res) =>{
-     const id =req.params.id;
-     const userIndex = users.findIndex((u) => u.id == id);
-     if(userIndex){
-          users.splice(userIndex,1)
-          res.status(200).json(users);
-     }
-     else {
-          res.status(404).json({message : "User is not Found"});
+app.delete('/users/:id',async(req,res) =>{
+     try {
+          const id =req.params.id;
+          const user = await User.findByIdAndDelete(id);
+          if(user){
+               res.status(200).json(user);
+          }
+          else {
+               res.status(404).json({message : "User is not Found"});
+          }
+     } catch (error) {
+          res.status(500).json({message : `SomeThing wrong in Server`});
      }
 
 })
